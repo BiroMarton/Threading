cca0efb16435136c034b4546e70598afc4a89217
BiroMarton <rezoner9@gmail.com>
2017-07-12 07:25:11 +0200

    Exercise 2 completed.

---
+++ SingleInstance/SingleInstance/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SingleInstance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mutex oneMutex = null;
+            const string MutexName = "RUNMEONCE";
+
+            try
+            {
+                oneMutex = Mutex.OpenExisting(MutexName);
+            }
+            catch (WaitHandleCannotBeOpenedException)
+            {
+                Console.WriteLine("Cannot open the mutex because it doesn't exist");
+            }
+
+            
+            if (oneMutex == null)
+            {
+                oneMutex = new Mutex(true, MutexName);
+                Console.WriteLine("Mutex created.");
+            }
+            else
+            {
+                oneMutex.Close();
+                Console.WriteLine("Mutex closed.");
+                return;
+            }
+
+            Console.Read();
+        }
+    }
+}
